b79336333166b960f11b1189681e1a39bd70353e
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-22 11:30:47 +0100

    Zadatak 2

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -2,11 +2,42 @@
     
   <div>
         <h1>Cars holder</h1>
+      <div>
+
+        <carList :cars="cars"></carList>
+
+      </div>
+
+
       </div>  
 </template>
 
 <script>
+import CarList from '../components/CarList.vue'
+import {cars}  from '../services/CarsServices'
+
+
 export default {
+  components: {
+    CarList
+  },
+
+  data(){
+    return {
+      cars: []
+    }
+  },
+   beforeRouteEnter (to, from, next) {
+    cars.getAll()
+      .then((response) => {
+        next((vm) => {
+          vm.cars = response.data
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+
 
 }
 </script>
---
+++ src/components/CarList.vue
@@ -0,0 +1,43 @@
+<template><div class="row">
+    <div class="card col-md-2" v-for="car in cars" :key="car.id">
+      <div class="card-body">
+        <h5 class="card-title">{{ car.brand }}</h5>
+        <p class="card-text">Model: {{ car.model }}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Year: {{ car.year }}</li>
+        <li class="list-group-item">Number of doors: {{ car.numberOfDoors }}</li>
+        <li class="list-group-item">Maximum speed: {{ car.maxSpeed }}</li>
+        <li class="list-group-item">{{ car.isAutomatic ? 'Automatic' : 'Manual' }}</li>
+        <li class="list-group-item">Engine: {{ capitalize(car.engine) }}</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Holder</a>
+        <a href="#" class="card-link">Holder</a>
+      </div>
+    </div>
+  </div>
+    
+</template>
+
+<script>
+
+import capitalize from 'lodash/capitalize'
+
+export default {
+    props: {
+        cars: Array
+    },
+
+    methods: {
+        capitalize(str){
+            return capitalize(str)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
